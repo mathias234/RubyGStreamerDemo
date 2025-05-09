becfb60c1e981129940da93507ecd7653d4c0482
mathias234 <mathiasda98@gmail.com>
2025-05-09 13:08:59 +0200

    Initial commit

---
+++ helpers.rb
@@ -0,0 +1,26 @@
+require 'securerandom'
+
+module Helpers
+  def make_element(element_type)
+    element = Gst::ElementFactory.make(element_type, "#{element_type}-#{SecureRandom.alphanumeric(16)}")
+    @elements ||= []
+    @elements << element
+
+    element
+  end
+
+  def decode_bin_link(src, video_sink_pad, audio_sink_pad)
+    src.signal_connect("pad-added") do |_, pad|
+      caps = pad.current_caps
+      structure = caps.get_structure(0)
+      mime_type = structure.name
+      pp mime_type
+
+      if mime_type.include?("video/x-raw")
+        pad.link(video_sink_pad)
+      elsif mime_type.include?("audio/x-raw")
+        pad.link(audio_sink_pad)
+      end
+    end
+  end
+end
